d217f4242725df80be5d21dc49f5ee5096878c89
emonkhan129 <emonkhan129@yahoo.com>
2016-10-07 00:02:12 +0600

    new project added

---
+++ Vehicle/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vehicle
+{
+    public partial class VehicleApplication : Form
+    {
+        Vehicle myVehicle = new Vehicle();
+        public VehicleApplication()
+        {
+            InitializeComponent();
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            string vehicleName = vehicleNameTextBox.Text;
+            string registrationNo = regNoTextBox.Text;
+
+            myVehicle.VehicleName = vehicleName;
+            myVehicle.RegistrationNo = registrationNo;
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            myVehicle.Speed = Convert.ToDouble(speedTextBox.Text);
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+           minSpeedTextBox.Text = myVehicle.GetMinimumSpeed().ToString();
+            maxSpeedTextBox.Text = myVehicle.GetMaximumSpeed().ToString();
+            avgSpeedTextBox.Text = myVehicle.GetAverageSpeed().ToString();
+        }
+    }
+}
---
+++ Vehicle/Vehicle.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    class Vehicle
+    {
+        public string VehicleName { set; get; }
+        public string RegistrationNo { set; get; }
+
+        private double speed;
+
+        private double totalSpeed ;
+        public double Speed
+        {
+            set
+            {
+                speed = value;
+                if (minSpeed < 1 && maxSpeed < 1)
+                {
+                    minSpeed = speed;
+                    maxSpeed = speed;
+                }
+                else
+                {
+                    if (minSpeed > speed)
+                    {
+                        minSpeed = speed;
+                    }
+                    if (maxSpeed < speed)
+                    {
+                        maxSpeed = speed;
+                    }
+                }
+                count++;
+            }
+            get
+            {
+                return speed;
+                
+            }
+        }
+        private double minSpeed;
+        private double maxSpeed;
+        private int count;
+
+        //private double MinSpeed
+        //{
+        //    set
+        //    {
+        //        if (minSpeed > Speed)
+        //        {
+        //            minSpeed = speed;
+        //        }
+        //        else
+        //        {
+        //            minSpeed = speed;
+        //        }
+               
+        //    }
+        //    get
+        //    {
+        //        return minSpeed;
+        //    }
+        //}
+
+        //private double MaxSpeed
+        //{
+        //    set
+        //    {
+        //        if (value < Speed)
+        //        {
+        //            maxSpeed = value;
+        //        }
+        //    }
+        //    get
+        //    {
+        //        return maxSpeed;
+        //    }
+        //}
+
+      
+
+        public double GetMinimumSpeed()
+        {
+            return minSpeed;
+        }
+
+        public double GetMaximumSpeed()
+        {
+            return maxSpeed;
+        }
+
+        public double CountTotalSpeed()
+        {
+            return totalSpeed += speed;
+        }
+        public double GetAverageSpeed()
+        {
+           // double countSpeed= totalSpeed += speed;
+            double result = CountTotalSpeed() / count;
+           
+             //   result = 0;
+            
+            return result;
+        }
+    }
+}
